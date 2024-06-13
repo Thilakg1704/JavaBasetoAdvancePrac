741f8b65eb5503c1a9df20941a87d6eec855e145
THILAKGATS <thilakrock1799@gmail.com>
2024-06-13 13:47:07 +0530

    Loop patterns

---
+++ src/main/java/loops/Pattern3.java
@@ -0,0 +1,20 @@
+package loops;
+
+public class Pattern3 {
+	public static void main(String args[]) {
+		
+		for(int i =1;i<=10;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i =1;i<=10;i++) {
+			for(int j=1;j<=10-i+1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ src/main/java/loops/Pattern5.java
@@ -0,0 +1,37 @@
+package loops;
+
+public class Pattern5 {
+	public static void main(String args[]) {
+		
+		for(int i =1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				if(j<=5-i) {
+					System.out.print("  ");
+				}
+				else
+					System.out.print("* ");
+			}
+			System.out.println();
+		}
+		System.out.println("--------------------------	");
+
+		for(int i =1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				if(i+j>5) {
+					System.out.print("* ");
+				}
+				else
+					System.out.print("  ");
+			}
+			System.out.println();
+		}
+		System.out.println("______________________________________");
+		
+		for(int i =1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+	}
+	}
+}
